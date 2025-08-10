4d16846240ac108162e3e27e4aa5a394d804771c
taichan-03 <tatataiyan3@gmail.com>
2025-08-10 17:53:32 +0900

    課題15

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // postsテーブルから全てのデータを取得し$postsに代入する
+        $posts = DB::table('posts')->get();
+        // posts.indexビューに$postsを渡して表示する
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,18 @@
     <title>Laravel基礎</title>
 </head>
 <body>
-    <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>タイトル</th>
+            <th>本文</th>
+        </tr>
+        @foreach ($posts as $post) 
+        <tr>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->content }}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 </html>
 
